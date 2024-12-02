d7cca43c513c8fc9496b6a6ae472972c3a4634f2
maxence.delehelle <maxence.delehelle@edu.esiee.fr>
2024-12-02 10:38:12 +0100

    return >>

--- game.py
+++ game.py
@@ -111,6 +111,8 @@ class Game:
 
         command_word = list_of_words[0]
 
+        if not command_word :
+            return
         # If the command is not recognized, print an error message
         if command_word not in self.commands.keys():
             print(f"\nCommande '{command_word}' non reconnue. Entrez 'help' pour voir la liste des commandes disponibles.\n")
