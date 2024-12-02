bc62f22d7b4c4ccaee89782c8d52f8ed17454291
maxence.delehelle <maxence.delehelle@edu.esiee.fr>
2024-12-02 10:33:59 +0100

    Update player.py

--- player.py
+++ player.py
@@ -9,15 +9,27 @@ class Player():
     
     # Define the move method.
     def move(self, direction):
-        # Get the next room from the exits dictionary of the current room.
-        next_room = self.current_room.exits[direction]
-
-        # If the next room is None, print an error message and return False.
-        if next_room is None:
-            print("\nAucune porte dans cette direction !\n")
-            return False
+       # Vérifier si la direction existe dans les sorties de la pièce actuelle
+        if direction in self.current_room.exits:
+        # Obtenir la pièce suivante
+            next_room = self.current_room.exits[direction]
         
-        # Set the current room to the next room.
+        # Si la direction mène à une sortie vide (None), afficher un message d'erreur
+            if next_room is None:
+                print("\nAucune porte dans cette direction !\n")
+                return False
+        
+        # Sinon, effectuer le déplacement vers la pièce suivante
+            self.current_room = next_room
+            return True
+        else:
+        # Si la direction n'existe pas dans les sorties, afficher un message d'erreur
+            print(f"Commande de direction non valide : '{direction}'")
+            return False
+
+
+
+    # Set the current room to the next room.
         self.current_room = next_room
         print(self.current_room.get_long_description())
         self.history.append(next_room.name)
